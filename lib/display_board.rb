43b99518bce3149598ec7dd824c640f41860e3a2
SULAIMAN - <sulaiman@ce.student.pens.ac.id>
2020-05-17 09:32:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,25 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board:
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  Welcome to Tic Tac Toe
+   |   |   
+-----------
+   |   |   
+-----------
+   |   |   
+What move would X like to make (1-9)?
+> 5
+   |   |   
+-----------
+   | X |   
+-----------
+   |   |   
+What move would O like to make (1-9)?
+> 1
+ 0 |   |   
+-----------
+   | X |   
+-----------
+   |   |   
+# etc...
 end
 
-display_board
